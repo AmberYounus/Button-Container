73604fe094aeee714478fb4a872b4b7d2109c2ec
Amber Younus <amberyounus22@gmail.com>
2023-09-11 23:15:14 +0500

    commit

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-class Rectangle {
-    constructor(w, h) {
-        this.w = w;
-        this.h = h;
-    }
-}
-
-// Write code that adds an 'area' method to the Rectangle class' prototype
-Rectangle.prototype.area = function () {
-    return this.w * this.h;
-}
-
-// Create a Square class that inherits from Rectangle and implement its class constructor
-class Square extends Rectangle{
-    constructor(s) {
-        super(s, s);
-    }
-}
-
-if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
-    const rec = new Rectangle(3, 4);
-    const sqr = new Square(3);
-    
-    console.log(rec.area());
-    console.log(sqr.area());
-} else {
-    console.log(-1);
-    console.log(-1);
-}
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
