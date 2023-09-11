a38798e8079b829bbe313807a832e125b0837de4
Amber Younus <amberyounus22@gmail.com>
2023-09-12 02:06:31 +0500

    commit

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ let btn6 = document.getElementById("btn6");
 let btn7 = document.getElementById("btn7");
 let btn8 = document.getElementById("btn8");
 let btn9 = document.getElementById("btn9");
+
+function clockwiserotation() {
+  [
+    btn1.innerHTML,
+    btn2.innerHTML,
+    btn3.innerHTML,
+    btn4.innerHTML,
+    btn6.innerHTML,
+    btn7.innerHTML,
+    btn8.innerHTML,
+    btn9.innerHTML,
+  ] = [
+    btn4.innerHTML,
+    btn1.innerHTML,
+    btn2.innerHTML,
+    btn7.innerHTML,
+    btn3.innerHTML,
+    btn8.innerHTML,
+    btn9.innerHTML,
+    btn6.innerHTML,
+  ];
+}
+btn5.addEventListener("click", clockwiserotation);
+
